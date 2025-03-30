14ef42bb8c3efffdbe24e3e24630a5088ea1c489
tak-akashi <tak.akashi@gmail.com>
2025-03-30 13:14:41 +0900

    updated

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 from dotenv import load_dotenv
 import os
-# from langchain.chat_models import ChatOpenAI
 from langchain.schema import SystemMessage, HumanMessage
 from langchain_openai import ChatOpenAI
 
